3c266c2503b23d220b224555cf07f7a1207e9211
wkargul <wojciech.kargul@gmail.com>
2022-01-26 16:07:38 +0100

    Management API impl with better error handling

---
+++ crates/ya-http-proxy-client/src/error.rs
@@ -0,0 +1,48 @@
+use actix_http::ResponseError;
+use awc::error::{PayloadError, SendRequestError};
+use http::uri::InvalidUri;
+use http::{Method, StatusCode};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("HTTP error requesting {method} {url}: {code}; msg: '{msg}'")]
+    SendRequestError {
+        code: StatusCode,
+        msg: String,
+        method: Method,
+        url: String,
+    },
+    #[error("JSON error: {0}")]
+    JsonError(#[from] serde_json::error::Error),
+    #[error("Invalid UTF8 string: {0}")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("AWC payload error: {0}")]
+    PayloadError(PayloadError),
+    #[error("Invalid URI string: {0}")]
+    InvalidUriError(InvalidUri),
+}
+
+impl From<PayloadError> for Error {
+    fn from(e: PayloadError) -> Self {
+        Error::PayloadError(e)
+    }
+}
+
+impl From<InvalidUri> for Error {
+    fn from(e: InvalidUri) -> Self {
+        Error::InvalidUriError(e)
+    }
+}
+
+impl Error {
+    pub(crate) fn from_request(err: SendRequestError, method: Method, url: String) -> Self {
+        let msg = err.to_string();
+        let code = err.status_code();
+        Error::SendRequestError {
+            code,
+            msg,
+            method,
+            url,
+        }
+    }
+}
