a5497ac26ea3b9d404ea39cd9901eb7be6b6a706
mfranciszkiewicz <marek@golem.network>
2022-04-01 00:00:22 +0200

    fix forwarded uri building

--- crates/ya-http-proxy/src/proxy/handler.rs
+++ crates/ya-http-proxy/src/proxy/handler.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 
 use hyper::client::HttpConnector;
 use hyper::header::{self, HeaderName, HeaderValue};
+use hyper::http::uri::PathAndQuery;
+use hyper::http::Uri;
 use hyper::{Body, Client, HeaderMap, Request, Response, StatusCode};
 use tokio::sync::RwLock;
 
@@ -75,7 +77,10 @@ pub async fn forward_req(
         headers.insert(HeaderName::from_static("x-forwarded-host"), host);
     }
 
-    *req.uri_mut() = proxy_to;
+    if let Err(e) = merge_path_and_query(req.uri_mut(), proxy_to) {
+        log::warn!("Forwarded path error: {}", e);
+        return response(StatusCode::INTERNAL_SERVER_ERROR);
+    }
     client.request(req).await
 }
 
@@ -85,6 +90,33 @@ fn response(code: StatusCode) -> hyper::Result<hyper::Response<hyper::Body>> {
         .status(code)
         .body(hyper::Body::empty())
         .unwrap())
+}
+
+#[inline]
+fn merge_path_and_query(req_uri: &mut Uri, proxy_to: Uri) -> Result<(), String> {
+    let mut to_parts = proxy_to.into_parts();
+
+    let to_paq = to_parts.path_and_query.as_ref();
+    let req_paq = req_uri.path_and_query();
+
+    match (to_paq, req_paq) {
+        (Some(_), Some(req)) if req == "/" => (),
+        (None, Some(req)) => {
+            to_parts.path_and_query.replace(req.clone());
+        }
+        (Some(to), Some(req)) if to == "/" => {
+            to_parts.path_and_query.replace(req.clone());
+        }
+        (Some(to), Some(req)) => {
+            let paq =
+                PathAndQuery::try_from(format!("{}{}", to, req)).map_err(|e| e.to_string())?;
+            to_parts.path_and_query.replace(paq);
+        }
+        _ => (),
+    }
+
+    *req_uri = Uri::from_parts(to_parts).map_err(|e| e.to_string())?;
+    Ok(())
 }
 
 #[inline]
@@ -114,4 +146,45 @@ fn extract_basic_auth(headers: &HeaderMap) -> Result<&str, ()> {
 fn extract_username(decoded_auth: &str) -> Result<&str, ()> {
     let mut split = decoded_auth.splitn(2, ':');
     split.next().ok_or(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::merge_path_and_query;
+    use hyper::http::Uri;
+
+    #[test]
+    fn merge_uri_paths() -> anyhow::Result<()> {
+        let verify = |against: Uri, uri: &str, expected: &str| {
+            let mut req_uri = Uri::try_from(uri).unwrap();
+            merge_path_and_query(&mut req_uri, against).unwrap();
+            assert_eq!(&req_uri.to_string(), expected);
+        };
+
+        let proxy_to = Uri::try_from("http://127.0.0.1").unwrap();
+        verify(proxy_to.clone(), "http://1.0.0.1", "http://127.0.0.1/");
+        verify(proxy_to.clone(), "http://1.0.0.1/", "http://127.0.0.1/");
+
+        let proxy_to = Uri::try_from("http://127.0.0.1/").unwrap();
+        verify(proxy_to.clone(), "http://1.0.0.1", "http://127.0.0.1/");
+        verify(proxy_to.clone(), "http://1.0.0.1/", "http://127.0.0.1/");
+
+        let proxy_to = Uri::try_from("http://127.0.0.1/to").unwrap();
+        verify(proxy_to.clone(), "http://1.0.0.1", "http://127.0.0.1/to");
+        verify(proxy_to.clone(), "http://1.0.0.1/", "http://127.0.0.1/to");
+
+        let proxy_to = Uri::try_from("http://127.0.0.1/to").unwrap();
+        verify(
+            proxy_to.clone(),
+            "http://1.0.0.1/resource",
+            "http://127.0.0.1/to/resource",
+        );
+        verify(
+            proxy_to.clone(),
+            "http://1.0.0.1/resource/",
+            "http://127.0.0.1/to/resource/",
+        );
+
+        Ok(())
+    }
 }
